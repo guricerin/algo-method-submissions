7e9e157451a74e2e695187b7bbf8d8585b962e3c
guricerin <chanbo1e9@gmail.com>
2021-08-20 15:38:34 +0900

    upd

---
+++ 03-logical-thinking-ability/01-basic-arithmetic/10.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+
+int main()
+{
+    cout << (2000 - (120 * 10) - 170) / 90 << "\n";
+}
---
+++ 03-logical-thinking-ability/01-basic-arithmetic/18.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+
+int main()
+{
+    cout << 12 * 2 - 4 << "\n";
+}
