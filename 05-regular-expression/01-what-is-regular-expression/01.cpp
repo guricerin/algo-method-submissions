13c79c4c5ac2b18a8da6ef92bc102d179c8fe0e7
guricerin <chanbo1e9@gmail.com>
2021-08-21 16:51:21 +0900

    upd

---
+++ 05-regular-expression/01-what-is-regular-expression/01.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+
+int main()
+{
+    string s;
+    cin >> s;
+    regex re(R"(.*algo.*)");
+    if (regex_match(s, re))
+    {
+        cout << "Yes\n";
+    }
+    else
+    {
+        cout << "No\n";
+    }
+}
