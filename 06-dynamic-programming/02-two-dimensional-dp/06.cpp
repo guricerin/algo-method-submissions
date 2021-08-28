10b6be941708de61de5e264f41068b683c65f283
guricerin <chanbo1e9@gmail.com>
2021-08-28 21:10:07 +0900

    upd

---
+++ 06-dynamic-programming/02-two-dimensional-dp/06.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+
+int main()
+{
+    int n;
+    cin >> n;
+    int as[n][n];
+    rep(i, 0, n) rep(j, 0, n) cin >> as[i][j];
+    int dp[n + 10][n + 10];
+    memset(dp, 0, sizeof(dp));
+
+    rep(i, 0, n) rep(j, 0, n)
+    {
+        if (i - 1 >= 0 && j - 1 >= 0)
+        {
+            auto a = max(dp[i - 1][j], dp[i][j - 1]);
+            dp[i][j] += a;
+        }
+        else if (i - 1 >= 0)
+        {
+            dp[i][j] += dp[i - 1][j];
+        }
+        else if (j - 1 >= 0)
+        {
+            dp[i][j] += dp[i][j - 1];
+        }
+        dp[i][j] += as[i][j];
+    }
+
+    cout << dp[n - 1][n - 1] << "\n";
+}
