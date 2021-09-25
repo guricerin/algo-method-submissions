64282bd5e77badff656a6fb4aa1daca6586fdaa4
guricerin <chanbo1e9@gmail.com>
2021-09-25 13:52:30 +0900

    jiriki

---
+++ 03-logical-thinking-ability/05-sum-and-diff/10.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+
+int main() {
+    cout << (186 + 12) / 2 << endl;
+}
