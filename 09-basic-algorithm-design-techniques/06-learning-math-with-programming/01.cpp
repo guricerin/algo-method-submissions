2fb6ec6faacf15c11087ac0cabfec76697474036
guricerin <chanbo1e9@gmail.com>
2021-10-29 13:03:43 +0900

    jiriki

---
+++ 09-basic-algorithm-design-techniques/06-learning-math-with-programming/01.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+
+int main() {
+    int n;
+    cin >> n;
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
+        ans += i;
+    }
+    cout << ans << endl;
+}
