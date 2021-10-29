687e424a5a7b3970d41bf9a2074aef3a52f2d0f5
guricerin <chanbo1e9@gmail.com>
2021-10-29 15:11:28 +0900

    jiriki

---
+++ 03-logical-thinking-ability/08-distribution/04.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+
+int main() {
+    int a, b, k;
+    cin >> a >> b >> k;
+    int y = (b - a) / (k + 1);
+    int x = b - y;
+    if (x >= 0 && y >= 0 && (b - a) % (k + 1) == 0) {
+        cout << x << endl;
+    } else {
+        cout << -1 << endl;
+    }
+}
