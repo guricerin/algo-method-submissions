f627530612a606c2a0aa0b6a483a3fa177f7c5ec
guricerin <chanbo1e9@gmail.com>
2021-11-02 15:15:14 +0900

    jiriki

---
+++ 08-number-theory/05-greatest-common-divisor/04.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+
+int main() {
+    i64 a, b;
+    cin >> a >> b;
+    cout << lcm(a, b) << endl;
+}
