5663e714042810beaf1ac399aa16079e05ae880f
guricerin <chanbo1e9@gmail.com>
2021-11-18 08:46:09 +0900

    jiriki

---
+++ 09-basic-algorithm-design-techniques/07-set/06.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+
+int main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    int x = n - k;
+    int y = m - k;
+    cout << x + y + k << endl;
+}
