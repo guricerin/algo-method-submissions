2db327de752721c7fb765a7ab4f5ef1302ca8018
guricerin <chanbo1e9@gmail.com>
2021-11-23 16:17:06 +0900

    jiriki

---
+++ 09-basic-algorithm-design-techniques/08-arithmetic-seq-and-geometric-seq/06.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+
+int main() {
+    i64 a = 1;
+    rep(i, 0, 21) {
+        cout << a << endl;
+        a *= 2;
+    }
+}
