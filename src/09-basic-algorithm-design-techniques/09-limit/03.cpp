c21a52083a89ddc3c1cb2ac23265735ef2f057a0
guricerin <chanbo1e9@gmail.com>
2022-01-21 15:11:13 +0900

    jiriki

---
+++ src/09-basic-algorithm-design-techniques/09-limit/03.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    string ans = "";
+    if (b == 0)
+        ans = to_string(a);
+    else if (b > 0)
+        ans = "inf";
+    else
+        ans = "-inf";
+    cout << ans << endl;
+}
