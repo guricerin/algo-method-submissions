cf7f4c39826e87d1cd337128226d7aa1ce9e4339
guricerin <chanbo1e9@gmail.com>
2022-01-24 16:02:02 +0900

    jiriki

---
+++ src/09-basic-algorithm-design-techniques/10-differentiation/03.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+
+int main() {
+    int a, b, c, k;
+    cin >> a >> b >> c >> k;
+
+    int m = 2 * a * k + b;
+    cout << m << endl;
+}
