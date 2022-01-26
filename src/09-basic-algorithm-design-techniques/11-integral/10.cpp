2e2f1e081bb0f09bab29d3bb8cce34e39939014a
guricerin <chanbo1e9@gmail.com>
2022-01-26 11:53:03 +0900

    jiriki

---
+++ src/09-basic-algorithm-design-techniques/11-integral/10.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+
+int main() {
+    int r;
+    cin >> r;
+    double ans = 4.0 * pow(r, 2);
+    cout << fixed << setprecision(10) << ans << endl;
+}
