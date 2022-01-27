45b8a4a7e168e86b76832bb61bb3588bc1ae0ca2
guricerin <chanbo1e9@gmail.com>
2022-01-27 18:07:37 +0900

    jiriki

---
+++ src/09-basic-algorithm-design-techniques/12-data-science-intro/19.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+
+/// 平均
+template <class T>
+double average(const vector<T>& ls) {
+    return accumulate(all(ls), 0.0) / (double)ls.size();
+}
+
+/// 分散 : データの散らばり具合
+/// 各要素に対する「データの平均値からの距離の二乗」の平均値
+/// 分散が0 -> データの値がすべて等しい
+template <class T>
+double variance(const vector<T>& ls) {
+    auto ave = average(ls);
+    double sum = 0;
+    for (auto l : ls) {
+        sum += pow(l - ave, 2);
+    }
+    return sum / (double)ls.size();
+}
+
+/// 標準偏差 : 分散の非負の平方根
+/// 分散と比較した場合、各データの単位と一致するというメリットがある
+template <class T>
+double std_deviation(const vector<T>& ls) {
+    auto omega = variance(ls);
+    return sqrt(omega);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> as(n);
+    rep(i, 0, n) cin >> as[i];
+
+    auto a0 = as[0];
+    sort(all(as));
+    auto ave = average(as);
+    auto sd = std_deviation(as);
+    double ans = 50 + 10.0 * (a0 - ave) / sd;
+    cout << fixed << setprecision(10) << ans << endl;
+}
