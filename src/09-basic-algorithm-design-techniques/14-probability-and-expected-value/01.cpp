9f08523f3297779792d127f45b5d19c4aa6f06b8
guricerin <chanbo1e9@gmail.com>
2022-01-29 21:19:48 +0900

    jiriki

---
+++ src/09-basic-algorithm-design-techniques/14-probability-and-expected-value/01.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+
+int main() {
+    int n;
+    cin >> n;
+    double ans = 1.0 / (double)n;
+    cout << fixed << setprecision(10) << ans << endl;
+}
