4bd74e5f2d46146b6695038d298e8519677372f4
guricerin <chanbo1e9@gmail.com>
2022-01-29 14:02:19 +0900

    jiriki

---
+++ src/09-basic-algorithm-design-techniques/13-number-of-cases/08.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+
+int main() {
+    int n;
+    cin >> n;
+    i64 ans = 1;
+    rep(i, 0, n) {
+        ans *= n - i;
+    }
+    cout << ans << endl;
+}
