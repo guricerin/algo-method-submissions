3b3cc26d883dcf1ca79464a9de06813bf0322e66
guricerin <chanbo1e9@gmail.com>
2022-04-04 18:04:38 +0900

    rename

---
+++ src/11_learning-math/08_probability-and-expected-value/17.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    double al = 2 * n;
+    // 偶数なら0点の可能性あり
+    if (n & 1 == 0) al += 1;
+}
