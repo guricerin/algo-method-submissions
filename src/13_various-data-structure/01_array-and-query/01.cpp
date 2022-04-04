6015e6fe174007daf2297b391cc45b5b4e387224
guricerin <chanbo1e9@gmail.com>
2022-04-05 08:26:36 +0900

    jiriki

---
+++ src/13_various-data-structure/01_array-and-query/01.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+
+int main() {
+    vector<int> as = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
+    cout << as[8] << endl;
+}
