68e084bdd7f78d33de25df1b20beecaf91eac8cd
guricerin <chanbo1e9@gmail.com>
2022-04-05 19:27:54 +0900

    jiriki

---
+++ src/13_various-data-structure/02_variable-length-array/06.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> as(n);
+    rep(i, 0, n) cin >> as[i];
+    int q;
+    cin >> q;
+    rep(i, 0, q) {
+        int t;
+        cin >> t;
+        if (t == 0) {
+            reverse(all(as));
+        } else if (t == 1) {
+            int v;
+            cin >> v;
+            as.push_back(v);
+        } else {
+            if (as.size() < 1) {
+                cout << "Error" << endl;
+            } else {
+                cout << as.back() << endl;
+                as.pop_back();
+            }
+        }
+    }
+}
