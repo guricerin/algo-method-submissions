007e5329f8deff7e1d30b68f825ba3dd314b7d72
guricerin <chanbo1e9@gmail.com>
2022-04-06 16:45:58 +0900

    jiriki

---
+++ src/13_various-data-structure/03_two-dim-array/06.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+
+int dxy[5][2] = {{-1, 0}, {0, -1}, {0, 0}, {0, 1}, {1, 0}};
+
+int main() {
+    int h, w;
+    cin >> h >> w;
+    vector<vector<int>> maze(h, vector<int>(w, 0));
+    int total = 0;
+    rep(i, 0, h) {
+        string s;
+        cin >> s;
+        rep(j, 0, w) {
+            if (s[j] == '#') {
+                maze[i][j] = 1;
+                total++;
+            }
+        }
+    }
+
+    int Q;
+    cin >> Q;
+    rep(i, 0, Q) {
+        int t;
+        cin >> t;
+        if (t == 0) {
+            int p, q;
+            cin >> p >> q;
+            rep(j, 0, 5) {
+                int ny = p + dxy[j][0], nx = q + dxy[j][1];
+                if (ny < 0 || h <= ny || nx < 0 || w <= nx) continue;
+                if (maze[ny][nx]) {
+                    total--;
+                } else {
+                    total++;
+                }
+                maze[ny][nx] = !maze[ny][nx];
+            }
+        } else {
+            cout << total << endl;
+        }
+    }
+}
