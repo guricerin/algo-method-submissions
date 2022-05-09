d533be50605f284ef7ca7da4c25c4a82f82e869e
guricerin <chanbo1e9@gmail.com>
2022-05-09 12:11:49 +0900

    jiriki

---
+++ src/14_various-data-structure/08_rooted-tree/04.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+
+using G = vector<set<int>>;
+
+void rec(const G& tree, int v) {
+    cout << v << " ";
+    for (auto& c : tree[v]) {
+        rec(tree, c);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    G tree = G(n);
+    rep(i, 1, n) {
+        int a;
+        cin >> a;
+        tree[a].insert(i);
+    }
+
+    rec(tree, 0);
+    cout << endl;
+}
