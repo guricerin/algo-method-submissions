dfdf4a76faf85b7735cacd34fbafa1e836226089
guricerin <chanbo1e9@gmail.com>
2022-06-22 17:58:46 +0900

    jiriki

---
+++ src/09_graph-algorithm/04_dfs/06.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define all(x) (x).begin(), (x).end()
+using i64 = int64_t;
+using G = vector<vector<int>>;
+
+void rec(const G& tree, vector<bool>& seen, int v) {
+    seen[v] = true;
+    for (auto u : tree[v]) {
+        if (seen[u]) {
+            continue;
+        }
+        rec(tree, seen, u);
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    auto tree = G(n);
+    rep(i, 0, m) {
+        int a, b;
+        cin >> a >> b;
+        tree[a].push_back(b);
+        tree[b].push_back(a);
+    }
+
+    map<vector<bool>, int> counts;
+    rep(i, 0, n) {
+        auto seen = vector<bool>(n, false);
+        rec(tree, seen, i);
+        counts[seen]++;
+    }
+    cout << counts.size() << endl;
+}
